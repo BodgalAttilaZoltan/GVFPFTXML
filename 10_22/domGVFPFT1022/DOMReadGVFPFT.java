ef7b707ca2ea47e27b89518d767a17b0844e3775
BodgalAttilaZoltan <bodgal.attila.zoltan@gmail.com>
2024-10-22 12:34:05 +0200

    Imports
    added imports

---
+++ 10_22/domGVFPFT1022/DOMReadGVFPFT.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParseConfigurationExcemption;
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class DOMRead {
+    
+}
